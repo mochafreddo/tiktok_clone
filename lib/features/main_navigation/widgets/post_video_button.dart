7bd51955889ed9ca42bf58da8ff3c8725d7ff209
mochafreddo <corc411@gmail.com>
2023-06-06 19:11:02 +0900

    #6.8 Post Video Button

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        // Positioned widget은 Stack 내부의 element를 위치시킨다.
+        // Positioned들은 Stack 안에서 정해진 크기가 있어야 한다.
+        // 기준점이 있어야 한다.
+        Positioned(
+          right: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 30,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size6,
+            ),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: 18,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
