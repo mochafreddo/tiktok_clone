ae5b800282525f5206fd6fc7ace4cf2e896b4c21
mochafreddo <corc411@gmail.com>
2023-06-06 18:38:55 +0900

    #6.7 Stateful Navigation part Two

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -14,14 +14,14 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    // statefull screen을 동시에 사용하면 GlobalKey를 사용해야 한다.
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-    Container(),
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-  ];
+  // statefull screen을 동시에 사용하면 GlobalKey를 사용해야 한다.
+  // final screens = [
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  //   Container(),
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  // ];
 
   void _onTap(int index) {
     setState(() {
@@ -34,12 +34,38 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     return Scaffold(
       // 아래 두 가지 방법은 사용자가 다른 화면으로 갈 때마다 index를 바꾸게 되고
       // 이전 화면은 완전히 없어진다. 그래서 다시 돌아와도 처음부터 화면을 만든다.
-      // 1.
-      // body: screens[_selectedIndex],
-      // 2.
-      body: screens.elementAt(_selectedIndex),
-      // 3. 사용자가 있던 위치를 기억하기를 원할 때
-      // 4. 화면을 폐기시켜 버리는 것이 아니라 잠시만 사라지게 하는 것은?
+      // 한 번에 하나의 화면만 렌더링한다.
+      // body: screens[_selectedIndex], // 1
+      // body: screens.elementAt(_selectedIndex), // 2
+
+      // 사용자가 있던 위치를 기억하기를 원할 때
+      // 화면을 폐기시켜 버리는 것이 아니라 잠시만 사라지게 하는 것
+      // 모든 화면을 그리긴 하지만 보이지는 않게, 즉 사용자에게 시각적으로 보이지 않게
+      // Offstage 위젯: widget이 안 보이게 하면서 계속 존재하게 한다.
+      body: Stack(
+        children: [
+          // Offstage widget과 Offstage children이 너무 많으면 모든 children이 동시에
+          // 활성화 되어 render 된다. 즉, 실수로 한 화면에서 너무 많은 resource를 사용한다면 그
+          // widget은 절대 없어지지 않을테니 앱 전체가 느려진다.
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: const StfScreen(),
+          ),
+        ],
+      ),
+
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
