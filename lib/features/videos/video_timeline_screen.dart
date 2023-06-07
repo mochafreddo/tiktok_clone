31fa0689f85286cfa7fb61a9ec36153d443071b4
mochafreddo <corc411@gmail.com>
2023-06-07 18:55:09 +0900

    #7.3 Video Player (14:09)

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -13,33 +14,49 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   final PageController _pageController =
       PageController(); // PageView를 제어하기 위한 컨트롤러
 
-  List<Color> colors = [
-    Colors.blue,
-    Colors.red,
-    Colors.yellow,
-    Colors.teal,
-  ];
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
+
+  // List<Color> colors = [
+  //   Colors.blue,
+  //   Colors.red,
+  //   Colors.yellow,
+  //   Colors.teal,
+  // ];
 
   void _onPageChanged(int page) {
     // animateToPage를 호출하면 페이지가 애니메이션으로 이동한다.
     _pageController.animateToPage(
       page,
-      duration: const Duration(milliseconds: 150),
-      curve: Curves.linear,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
 
-    // 마지막 페이지에 도달하면 새로운 색상을 추가한다.
     if (page == _itemCount - 1) {
       _itemCount = _itemCount + 4;
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.teal,
-      ]);
+
+      // colors.addAll([
+      //   Colors.blue,
+      //   Colors.red,
+      //   Colors.yellow,
+      //   Colors.teal,
+      // ]);
 
       setState(() {}); // setState를 호출하면 PageView가 다시 그려진다.
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -61,16 +78,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical, // 페이지가 세로로 스크롤되게 한다.
       onPageChanged: _onPageChanged, // 페이지가 바뀔 때마다 호출된다.
       itemCount: _itemCount, // 페이지의 개수를 지정한다.
-      itemBuilder: (context, index) => Container(
-        color: colors[index],
-        child: Center(
-          child: Text(
-            "Screen $index",
-            style: const TextStyle(
-              fontSize: 68,
-            ),
-          ),
-        ),
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
       ),
     );
   }
