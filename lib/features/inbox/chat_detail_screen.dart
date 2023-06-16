a126003bbc4558ad53e68be244211f867cfe2861
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-06-16 18:02:05 +0900

    #11.4 Chat Detail: Code Challenge

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -15,23 +15,43 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const ListTile(
+        title: ListTile(
           contentPadding: EdgeInsets.zero, // ListTile의 padding을 없애기 위해
           horizontalTitleGap: Sizes.size8, // 타이틀과 leading 사이의 간격
-          leading: CircleAvatar(
-            radius: Sizes.size24,
-            foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/76798197?v=4"),
-            child: Text("니꼬"),
+          leading: Stack(
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size24,
+                foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/76798197?v=4"),
+                child: Text("니꼬"),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: Sizes.size17,
+                  height: Sizes.size17,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: Sizes.size3,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          title: Text(
+          title: const Text(
             "니꼬",
             style: TextStyle(
               fontWeight: FontWeight.w600,
             ),
           ),
-          subtitle: Text("Active now"),
-          trailing: Row(
+          subtitle: const Text("Active now"),
+          trailing: const Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               FaIcon(
@@ -97,16 +117,62 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
             bottom: 0,
             width: MediaQuery.of(context).size.width, // 화면의 가로 길이
             child: BottomAppBar(
-              color: Colors.grey.shade50,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size18,
+                vertical: Sizes.size9,
+              ),
+              color: Colors.grey.shade200,
               child: Row(
                 children: [
-                  const Expanded(
+                  Expanded(
                     // 화면의 가로 길이를 차지하는 위젯
-                    child: TextField(),
+                    child: SizedBox(
+                      height: Sizes.size44,
+                      child: TextField(
+                        style: const TextStyle(
+                          fontSize: Sizes.size16,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Send a message...",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(Sizes.size20),
+                              topRight: Radius.circular(Sizes.size20),
+                              bottomLeft: Radius.circular(Sizes.size20),
+                            ),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: const EdgeInsets.only(
+                            left: Sizes.size16,
+                          ),
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FaIcon(
+                                FontAwesomeIcons.faceSmile,
+                                color: Colors.grey.shade500,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  Gaps.h20,
+                  Gaps.h14,
                   Container(
-                    child: const FaIcon(FontAwesomeIcons.paperPlane),
+                    padding: const EdgeInsets.all(Sizes.size9),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(Sizes.size32),
+                    ),
+                    child: const FaIcon(
+                      FontAwesomeIcons.paperPlane,
+                      color: Colors.white,
+                      size: Sizes.size20 + Sizes.size2,
+                    ),
                   )
                 ],
               ),
--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/discover/activity_screen.dart';
+import 'package:tiktok_clone/features/inbox/activity_screen.dart';
 import 'package:tiktok_clone/features/inbox/chats_screen.dart';
 
 class InboxScreen extends StatefulWidget {
