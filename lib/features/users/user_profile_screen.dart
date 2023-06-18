c2b55389ef85e8595435b6c5b6d720074330e79f
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-06-18 16:12:59 +0900

    #12.3 SliverGrid (04:06)

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -13,8 +14,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          snap: true,
-          floating: true,
+          // snap: true,
+          // floating: true,
           pinned: true,
           stretch: true,
           backgroundColor: Colors.teal,
@@ -34,7 +35,9 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           ),
         ),
         SliverFixedExtentList(
+          // SliverFixedExtentList: 고정된 크기의 리스트
           delegate: SliverChildBuilderDelegate(
+            // SliverChildBuilderDelegate: 리스트 아이템을 생성하는 델리게이트
             childCount: 50, // 아이템 개수
             (context, index) => Container(
               color: Colors.amber[100 * (index % 9)],
@@ -45,6 +48,26 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           itemExtent: 100, // 높이
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            // SliverChildBuilderDelegate: 그리드 아이템을 생성하는 델리게이트
+            childCount: 50, // childCount: 아이템 개수
+            (context, index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            // SliverGridDelegateWithMaxCrossAxisExtent: 가로 방향으로 최대 크기를 가진 그리드
+            maxCrossAxisExtent: 100, // maxCrossAxisExtent: 그리드의 최대 크기
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1, // childAspectRatio: 그리드의 가로 세로 비율
+          ),
         )
       ],
     );
