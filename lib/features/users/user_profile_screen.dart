b53ded1deee3c6d5a8a682ba791d59ef9e34b4a3
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-06-18 18:11:23 +0900

    #12.4 SliverPersistentHeader (10:33)

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -12,11 +12,14 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      // CustomScrollView: 스크롤 가능한 위젯
       slivers: [
+        // slivers 안에는 wedget을 그냥 갖다넣을 수 없다.
+        // slivers를 그냥 '스크롤 가능한 구역'이라고 생각하면 된다.
         SliverAppBar(
           // snap: true,
           // floating: true,
-          pinned: true,
+          // pinned: true,
           stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
@@ -34,6 +37,17 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             title: const Text("Hello!"),
           ),
         ),
+        const SliverToBoxAdapter(
+          // SliverToBoxAdapter: 박스 위젯을 슬리버로 감싸는 위젯
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 20,
+              ),
+            ],
+          ),
+        ),
         SliverFixedExtentList(
           // SliverFixedExtentList: 고정된 크기의 리스트
           delegate: SliverChildBuilderDelegate(
@@ -48,6 +62,12 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           itemExtent: 100, // 높이
+        ),
+        SliverPersistentHeader(
+          // SliverPersistentHeader: 스크롤을 해도 사라지지 않는 헤더
+          delegate: CustomDelegate(),
+          // pinned: true,
+          floating: true,
         ),
         SliverGrid(
           delegate: SliverChildBuilderDelegate(
@@ -71,5 +91,47 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         )
       ],
     );
+  }
+}
+
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(
+      color: Colors.indigo,
+      child: const FractionallySizedBox(
+        // FractionallySizedBox: 자식 위젯의 크기를 비율로 지정
+        // 부모로부터 최대한 많은 공간을 차지
+        heightFactor: 1, // heightFactor: 높이 비율
+        child: Center(
+          child: Text(
+            "Title!!!!!",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 150;
+  // maxExtent: 헤더의 최대 높이
+
+  @override
+  double get minExtent => 80;
+  // minExtent: 헤더의 최소 높이
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // shouldRebuild: 헤더를 다시 빌드해야 하는지 여부를 반환
+    // true를 반환하면 헤더를 다시 빌드
+    // false를 반환하면 헤더를 다시 빌드하지 않음
+    return false;
   }
 }
