163f8f541835f99562589144478d7b011dc05e0f
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-06-21 18:18:23 +0900

    #13.0 ListWheelScrollView (08:26)

---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      /* body: const Column(children: [
+        CloseButton() // CloseButton: 닫기 버튼
+      ]), */
+      /* body: ListWheelScrollView(
+        // ListWheelScrollView: 스크롤 가능한 위젯
+        // useMagnifier: true, // useMagnifier: 확대 기능 사용 여부
+        // magnification: 1.5, // magnification: 확대 비율
+        diameterRatio: 1.5, // diameterRatio: 지름 비율
+        offAxisFraction: 2, // offAxisFraction: 스크롤 시 회전하는 정도
+        itemExtent: 200, // itemExtent: 각 아이템의 높이
+        children: [
+          for (var x in [
+            1,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ])
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                color: Colors.teal,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Pick me",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 39,
+                  ),
+                ),
+              ),
+            )
+        ],
+      ), */
+      body: const Column(
+        children: [
+          /* CupertinoActivityIndicator(
+            // CupertinoActivityIndicator: iOS 스타일의 로딩 표시기
+            radius: 40,
+            // animating: false,
+          ), */
+          /* CircularProgressIndicator(), // CircularProgressIndicator: 원형 로딩 표시기 */
+          CircularProgressIndicator
+              .adaptive(), // CircularProgressIndicator.adaptive(): 플랫폼에 맞는 로딩 표시기
+        ],
+      ),
+    );
+  }
+}
