5559811cc813a26f9d4d3fdf8b18e8512b9dedd4
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-06-22 16:08:36 +0900

    #13.1 AboutListTile (04:21)

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -6,13 +6,13 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      /* body: const Column(children: [
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+        /* body: const Column(children: [
         CloseButton() // CloseButton: 닫기 버튼
       ]), */
-      /* body: ListWheelScrollView(
+        /* body: ListWheelScrollView(
         // ListWheelScrollView: 스크롤 가능한 위젯
         // useMagnifier: true, // useMagnifier: 확대 기능 사용 여부
         // magnification: 1.5, // magnification: 확대 비율
@@ -58,7 +58,7 @@ class SettingsScreen extends StatelessWidget {
             )
         ],
       ), */
-      body: const Column(
+        /* body: const Column(
         children: [
           /* CupertinoActivityIndicator(
             // CupertinoActivityIndicator: iOS 스타일의 로딩 표시기
@@ -69,7 +69,26 @@ class SettingsScreen extends StatelessWidget {
           CircularProgressIndicator
               .adaptive(), // CircularProgressIndicator.adaptive(): 플랫폼에 맞는 로딩 표시기
         ],
-      ),
-    );
+      ), */
+        body: ListView(
+          children: const [
+            /* ListTile(
+              onTap: () => showAboutDialog( // showAboutDialog: AboutDialog를 표시하는 함수
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reserved. Please don't copy me.",
+              ),
+              title: const Text(
+                "About",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: const Text("About this app....."),
+            ), */
+            AboutListTile(), // AboutListTile: AboutDialog를 표시하는 위젯
+          ],
+        ));
   }
 }
