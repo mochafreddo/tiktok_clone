205f2fda71f276001652d31fc004be139b4d6bd5
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-07-05 12:31:09 +0900

    feat: improve code readability and reusability in TikTokApp theme
    - Extract common color and TextStyle to static constants for reusability
    - Add comments to main function and TikTokApp class for clarity
    - Rearrange ThemeData properties order to match between theme and darkTheme
    - Add Key parameter to TikTokApp constructor
    - Remove redundant comment
    
    BREAKING CHANGE: None

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,14 @@ import 'package:tiktok_clone/generated/l10n.dart';
 import 'package:tiktok_clone/router.dart';
 
 void main() async {
-  /// Flutter framework를 이용해 앱이 시작하기 전에 state를 어떤 식으로든 바꾸고 싶다면 engine 자체와 engine과 widget의 연결을 확실하게 초기화시켜야 한다.
-  /// 이렇게 하면 모든 widget들이 engine과 연결된 것을 확실하게 보장해줄 수 있다.
-  /// WidgetsFlutterBinding: This is the glue that binds the framework to the Flutter engine.
-  /// ensureInitialized(): You only need to call this method if you need the binding to be initialized before calling [runApp].
+  // Ensure that widget binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  /// 앱이 시작하기 전에 화면 방향을 portrait로 고정시킨다.
-  await SystemChrome.setPreferredOrientations(
-    [
-      DeviceOrientation.portraitUp,
-    ],
-  );
+  // Set preferred orientations for the app
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  /// 앱이 시작하기 전에 상태바의 색상을 검은색으로 고정시킨다.
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle.dark,
-  );
+  // Set System UI Overlay Style
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
   runApp(const TikTokApp());
 }
@@ -30,144 +21,52 @@ void main() async {
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
 
+  // Define the color used in multiple places in the theme
+  static const Color primaryColor = Color(0xFFE9435A);
+
+  // Define TextStyle used in AppBar Theme
+  static const TextStyle appBarTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: Sizes.size16 + Sizes.size2,
+    fontWeight: FontWeight.w600,
+  );
+
   @override
   Widget build(BuildContext context) {
-    // S.load(const Locale("en"));
-    // return MaterialApp(
     return MaterialApp.router(
       routerConfig: router,
-      debugShowCheckedModeBanner: false, // 디버그 배너 제거
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       localizationsDelegates: const [
-        // 여기서 delegate란 일종의 번역 파일들로 생각하면 된다.
-
-        S.delegate, // 앱의 번역 파일
-
-        // AppLocalizations.delegate의 좋은 점은
-        // supportedLocales의 리스트도 함께 제공한다는 것.
-        // AppLocalizations.delegate, // 앱의 번역 파일
-
-        // Flutter에는 텍스트가 기본적으로 들어가 있는 위젯들이 있다.
-        // Flutter는 그런 위젯들의 번역이 이미 해놓았다.
-        // 그래서 아래처럼 Flutter가 기본으로 가지고 있는 위젯들에 대한 번역들을 include 하는 것.
+        S.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      // localizationsDelegates: AppLocalizations.localizationsDelegates, // 위의 주석과 같은 코드
-      // 앱이 어떤 언어를 지원하는 지 알려줘야 한다.
-      // ref: IANA Language Subtag Registry
-      supportedLocales: const [
-        Locale("en"),
-        Locale("ko"),
-        // Locale("es"),
-      ],
-      // supportedLocales: AppLocalizations.supportedLocales, // 위의 주석과 같은 코드
-      themeMode: ThemeMode.system, // ThemeMode.system: 시스템의 테마를 따라간다.
+      supportedLocales: const [Locale("en"), Locale("ko")],
+      themeMode: ThemeMode.system,
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
-
-        // This [TextTheme] provides color
-        // but not geometry (font size, weight, etc).
         textTheme: Typography.blackMountainView,
-
-        // textTheme: GoogleFonts.itimTextTheme(),
-        /* textTheme: TextTheme(
-          displayLarge: GoogleFonts.openSans(
-            fontSize: 96,
-            fontWeight: FontWeight.w300,
-            letterSpacing: -1.5,
-          ),
-          displayMedium: GoogleFonts.openSans(
-            fontSize: 60,
-            fontWeight: FontWeight.w300,
-            letterSpacing: -0.5,
-          ),
-          displaySmall: GoogleFonts.openSans(
-            fontSize: 48,
-            fontWeight: FontWeight.w400,
-          ),
-          headlineMedium: GoogleFonts.openSans(
-            fontSize: 34,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.25,
-          ),
-          headlineSmall: GoogleFonts.openSans(
-            fontSize: 24,
-            fontWeight: FontWeight.w400,
-          ),
-          titleLarge: GoogleFonts.openSans(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 0.15,
-          ),
-          titleMedium: GoogleFonts.openSans(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.15,
-          ),
-          titleSmall: GoogleFonts.openSans(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 0.1,
-          ),
-          bodyLarge: GoogleFonts.roboto(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.5,
-          ),
-          bodyMedium: GoogleFonts.roboto(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.25,
-          ),
-          labelLarge: GoogleFonts.roboto(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1.25,
-          ),
-          bodySmall: GoogleFonts.roboto(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 0.4,
-          ),
-          labelSmall: GoogleFonts.roboto(
-            fontSize: 10,
-            fontWeight: FontWeight.w400,
-            letterSpacing: 1.5,
-          ),
-        ), */
         scaffoldBackgroundColor: Colors.white,
-        /* bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.grey.shade50,
-        ), */
-        primaryColor: const Color(0xFFE9435A),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color(0xFFE9435A), // 커서 색상
-          // selectionColor: Color(0xFFE9435A), // 선택된 영역의 배경색
-        ),
+        primaryColor: primaryColor,
+        textSelectionTheme:
+            const TextSelectionThemeData(cursorColor: primaryColor),
         splashColor: Colors.transparent,
-        // highlightColor: Colors.transparent,
         appBarTheme: const AppBarTheme(
           foregroundColor: Colors.black,
           backgroundColor: Colors.white,
           surfaceTintColor: Colors.white,
           elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-          ),
+          titleTextStyle: appBarTextStyle,
         ),
         tabBarTheme: TabBarTheme(
           indicatorColor: Colors.black,
           labelColor: Colors.black,
           unselectedLabelColor: Colors.grey.shade500,
         ),
-        listTileTheme: const ListTileThemeData(
-          iconColor: Colors.black,
-        ),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.black),
       ),
       darkTheme: ThemeData(
         useMaterial3: true,
@@ -176,81 +75,21 @@ class TikTokApp extends StatelessWidget {
           labelColor: Colors.white,
           unselectedLabelColor: Colors.grey.shade700,
         ),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color(0xFFE9435A),
-        ),
+        textSelectionTheme:
+            const TextSelectionThemeData(cursorColor: primaryColor),
         textTheme: Typography.whiteMountainView,
-        /* textTheme: GoogleFonts.itimTextTheme(
-          ThemeData(
-            brightness: Brightness.dark,
-          ).textTheme,
-        ), */
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
         appBarTheme: AppBarTheme(
           surfaceTintColor: Colors.grey.shade900,
           backgroundColor: Colors.grey.shade900,
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-          ),
-          actionsIconTheme: IconThemeData(
-            color: Colors.grey.shade100,
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.grey.shade100,
-          ),
+          titleTextStyle: appBarTextStyle.copyWith(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.grey.shade100),
+          iconTheme: IconThemeData(color: Colors.grey.shade100),
         ),
-        bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.grey.shade900,
-        ),
-        primaryColor: const Color(0xFFE9435A),
+        bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade900),
+        primaryColor: primaryColor,
       ),
-
-      // home: const SignUpScreen(),
-      // home: const MainNavigationScreen(),
-      // home: const ActivityScreen(),
-      // home: const LayoutBuilderCodeLab(),
-      // home: const SettingsScreen(),
-      /* initialRoute: SignUpScreen.routeName,
-      routes: {
-        SignUpScreen.routeName: (context) => const SignUpScreen(),
-        UsernameScreen.routeName: (context) => const UsernameScreen(),
-        LoginScreen.routeName: (context) => const LoginScreen(),
-        EmailScreen.routeName: (context) => const EmailScreen(),
-      }, */
     );
   }
 }
-
-/* class LayoutBuilderCodeLab extends StatelessWidget {
-  const LayoutBuilderCodeLab({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SizedBox(
-        width: size.width / 2,
-        child: LayoutBuilder(
-          builder: (context, constraints) => Container(
-            // constraints: BoxConstraints box가 커질 수 있는 최대치를 알려준다.
-            width: constraints.maxWidth,
-            height: constraints.maxHeight,
-            color: Colors.teal,
-            child: Center(
-              child: Text(
-                "${size.width} / ${constraints.maxWidth}",
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 98,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-} */
