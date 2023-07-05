85bb2cbeb3a4ef4ea1a1747492f76d3535204be4
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-07-05 18:31:01 +0900

    #20.6 InheritedWidget (14:48)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tiktok_clone/common/widgets/video_config/video_config.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
 import 'package:tiktok_clone/router.dart';
@@ -33,62 +34,64 @@ class TikTokApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      title: 'TikTok Clone',
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale("en"), Locale("ko")],
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        textTheme: Typography.blackMountainView,
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: primaryColor,
-        textSelectionTheme:
-            const TextSelectionThemeData(cursorColor: primaryColor),
-        splashColor: Colors.transparent,
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: appBarTextStyle,
+    return VideoConfig(
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        title: 'TikTok Clone',
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale("en"), Locale("ko")],
+        themeMode: ThemeMode.system,
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.light,
+          textTheme: Typography.blackMountainView,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: primaryColor,
+          textSelectionTheme:
+              const TextSelectionThemeData(cursorColor: primaryColor),
+          splashColor: Colors.transparent,
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: appBarTextStyle,
+          ),
+          tabBarTheme: TabBarTheme(
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+          ),
+          listTileTheme: const ListTileThemeData(iconColor: Colors.black),
         ),
-        tabBarTheme: TabBarTheme(
-          indicatorColor: Colors.black,
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey.shade500,
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          tabBarTheme: TabBarTheme(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey.shade700,
+          ),
+          textSelectionTheme:
+              const TextSelectionThemeData(cursorColor: primaryColor),
+          textTheme: Typography.whiteMountainView,
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Colors.black,
+          appBarTheme: AppBarTheme(
+            surfaceTintColor: Colors.grey.shade900,
+            backgroundColor: Colors.grey.shade900,
+            titleTextStyle: appBarTextStyle.copyWith(color: Colors.white),
+            actionsIconTheme: IconThemeData(color: Colors.grey.shade100),
+            iconTheme: IconThemeData(color: Colors.grey.shade100),
+          ),
+          bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade900),
+          primaryColor: primaryColor,
         ),
-        listTileTheme: const ListTileThemeData(iconColor: Colors.black),
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        tabBarTheme: TabBarTheme(
-          indicatorColor: Colors.white,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.grey.shade700,
-        ),
-        textSelectionTheme:
-            const TextSelectionThemeData(cursorColor: primaryColor),
-        textTheme: Typography.whiteMountainView,
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(
-          surfaceTintColor: Colors.grey.shade900,
-          backgroundColor: Colors.grey.shade900,
-          titleTextStyle: appBarTextStyle.copyWith(color: Colors.white),
-          actionsIconTheme: IconThemeData(color: Colors.grey.shade100),
-          iconTheme: IconThemeData(color: Colors.grey.shade100),
-        ),
-        bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade900),
-        primaryColor: primaryColor,
       ),
     );
   }
--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/common/widgets/main_navigation_screen.dart';
+import 'package:tiktok_clone/common/widgets/main_navigation/main_navigation_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/inbox/activity_screen.dart';
